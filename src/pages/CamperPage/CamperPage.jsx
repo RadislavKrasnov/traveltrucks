d8b4bb2df76cd449f0e0da1db33c5d546a74c72d
Radyslav Krasnov <radislavkrasnoff@gmail.com>
2025-03-19 09:52:17 +0200

    add pages add router

---
+++ src/pages/CamperPage/CamperPage.jsx
@@ -0,0 +1,5 @@
+const CamperPage = () => {
+    return <p>Camper</p>;
+}
+
+export default CamperPage;
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return <p>Not found</p>;
+};
+
+export default NotFoundPage;
