aaa8e777a19eae5d5551a4e1125a9a07555ea5ce
Radyslav Krasnov <radislavkrasnoff@gmail.com>
2025-03-23 18:32:02 +0200

    add not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
+import css from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
-  return <p>Not found</p>;
+  return (
+    <div className={css.container}>
+      <p>404 Not found</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
