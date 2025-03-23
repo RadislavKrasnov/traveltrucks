d71beca3e6df1977733a9127f99430b356718c3d
Radyslav Krasnov <radislavkrasnoff@gmail.com>
2025-03-23 17:30:08 +0200

    add rating and reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,16 +1,44 @@
 import { useSelector } from 'react-redux';
 import { selectCamper } from '../../redux/campersSlice';
 import css from './Reviews.module.css';
+import Icon from '../Icon/Icon';
 
 const Reviews = () => {
   const camper = useSelector(selectCamper);
   const reviews = camper.reviews;
 
+  const showStarReting = reviewer_rating => {
+    const totalStars = 5;
+    const goldStar = <Icon id="gold-star" width={16} height={16} />;
+    const grayStar = <Icon id="gray-star" width={16} height={16} />;
+
+    return (
+      <div className={css.starBar}>
+        {Array.from({ length: totalStars }, (_, i) => (
+          <span key={i}>{i < reviewer_rating ? goldStar : grayStar}</span>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className={css.reviews}>
-      <ul>
-        {reviews.map((review, index) => (
-          <li key={index}>{JSON.stringify(review)}</li>
+      <ul className={css.reviewsList}>
+        {reviews.map(({ reviewer_name, reviewer_rating, comment }, index) => (
+          <li key={index}>
+            <div className="reviewItem">
+              <div className={css.author}>
+                <div className={css.avatar}>
+                  {reviewer_name.charAt(0).toUpperCase()}
+                </div>
+                <div>
+                  <p className={css.name}>{reviewer_name}</p>
+                  <div>{showStarReting(reviewer_rating)}</div>
+                </div>
+              </div>
+              <div className={css.comment}>{comment}</div>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
